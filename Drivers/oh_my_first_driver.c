97b5de9067bdb73ecfd779cc87a52c217f8d73a9
cvam0000 <cvam0000@gmail.com>
2019-12-12 18:04:18 +0530

    first device driver

---
+++ Drivers/oh_my_first_driver.c
@@ -0,0 +1,20 @@
+#include<linux/init.h>
+#include<linux/module.h>
+
+int cvam_module_init(void)
+{
+	printk(KERN_ALERT " Hello world %s im inside your kernel \n"__FUNCTION__);
+	return 0;
+
+	}
+
+void cvam_module_exit(void)
+
+{
+	printk(KERN_ALERT " Have to leave the kernel %s ", __Function__);
+
+}
+
+module_init(cvam_module_inti);
+module_exit(cvam_module_exit);
+

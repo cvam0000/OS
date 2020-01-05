bbd7bfe5e49963441bd5c0b8e3fd0a07fabad6ff
cvam0000 <cvam0000@gmail.com>
2020-01-05 23:00:05 +0530

    dynamically ans static way to allocate the major and minor no

--- Drivers/Char_Device_Drivers/Dynamically_Allocation_Major_Minor.c
+++ Drivers/Char_Device_Drivers/Dynamically_Allocation_Major_Minor.c
@@ -7,11 +7,15 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("CVAM");
 MODULE_VERSION("0.1");
 
-dev_t dev=MKDEV(235,0); // dev is the dev_t structure variable for major and minor no.
+dev_t dev=0; 
 
 static int static_init_no(void)
 {
-    register_chrdev_region(dev,1,"ohmyarch_dev");
+    if(alloc_chrdev_region(&dev,0,1,"ohmyarch_dev")<0)
+    {
+        printk(KERN_ALERT "NOT ABLE TO ALLOCATE THE MAJOR NO FOR THE DEVICE");
+        return -1;
+    }
     printk(KERN_INFO "Major = %d Minor = %d \n", MAJOR(dev),MINOR(dev));
     printk(KERN_ALERT "Module Inserted Successfully");
     return 0;
